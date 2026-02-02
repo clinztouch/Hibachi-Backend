4fb17f287df275a79f0395c27a93553a7bd80d45
clinztouch <iordyecalvin@gmail.com>
2026-02-02 23:51:34 +0100

    Delete frontend directory

--- frontend/modules/config.js
+++
@@ -1,5 +0,0 @@
-// Automatically switches between local and production
-export const API_BASE =
-  window.location.hostname === 'localhost'
-    ? 'http://localhost:5000'
-    : 'https://hibachiapp.onrender.com';
--- frontend/modules/utils.js
+++
@@ -1,8 +0,0 @@
-export function showSpinner() {
-    document.getElementById('globalSpinner')?.classList.remove('hidden');
-  }
-  
-  export function hideSpinner() {
-    document.getElementById('globalSpinner')?.classList.add('hidden');
-  }
-  
